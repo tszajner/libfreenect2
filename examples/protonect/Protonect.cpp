7cc3a32c614ccafa266c5cd7f30cfea61ee7be76
tszajner <tszajner@gmail.com>
2015-11-02 04:06:51 +0000

    Combined some features. Also, we switched the order of zooming and edge detection

--- examples/protonect/Protonect.cpp
+++ examples/protonect/Protonect.cpp
@@ -47,9 +47,14 @@ bool protonect_shutdown = false;
 typedef enum { IR, Depth, DepthEdge, IREdge } State;
 
 //Some Canny Threshold
-int const max_lowThreshold = 100;
-int ratio = 3;
+int const ratio = 3;
+int const cannythresh = 50;
 //
+
+//Zoom scale
+float scale = 100;
+int const STREAM_HEIGHT = 424;
+int const STREAM_WIDTH = 512;
 
 void sigint_handler(int s)
 {
@@ -65,13 +70,13 @@ int main(int argc, char *argv[])
 
   std::string binpath = "/";
   State stream = IR; 
-  jetsonGPIO input = gpio166 ;
+  /*jetsonGPIO input = gpio166 ;
   gpioExport(input);
   gpioOpen(input);
   gpioSetDirection(input, inputPin);
   //gpioSetEdge(input, "rising");
   unsigned int value = 0;
-  unsigned int trigger = 0;
+  unsigned int trigger = 0; */
   if(executable_name_idx != std::string::npos)
   {
     binpath = program_path.substr(0, executable_name_idx);
@@ -99,7 +104,10 @@ int main(int argc, char *argv[])
 
   std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
   std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
-
+  cv::namedWindow("Kinect", CV_WINDOW_NORMAL);
+  
+  //int pos, threshold = 0;
+  //cv::createTrackbar("EdgeThresholds", "Kinect", &threshold, max_lowThreshold);
   while(!protonect_shutdown)
   {
     listener.waitForNewFrame(frames);
@@ -117,41 +125,78 @@ int main(int argc, char *argv[])
     cv::imshow("rgb", bgra);
 #endif
 */
+
+    float height = float(STREAM_HEIGHT) * (scale / 100.0f); 
+    float width = float(STREAM_WIDTH) * (scale / 100.0f);
+    int a,b,c,d;
+    a = (STREAM_WIDTH - int(width)) / 2;
+    b = STREAM_WIDTH - 1 - (STREAM_WIDTH - int(width)) / 2;
+    c = (STREAM_HEIGHT - int(height)) / 2;
+    d = STREAM_HEIGHT - 1 - (STREAM_HEIGHT - int(height)) / 2;
     switch (stream)
     {
         case IR:
 	{
-               cv::imshow("Kinect", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
+
+              cv::Mat IR =  cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 4500.0f;
+              IR  = IR(cv::Range(c, d), cv::Range(a,b));
+              cv::resize(IR, IR, IR.size()); 
+              cv::imshow("Kinect", IR);
 	}       
 	break;
         case Depth:
 	{
-               cv::imshow("Kinect", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
+              cv::Mat Depth =  cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f;
+              Depth = Depth(cv::Range(c, d), cv::Range(a,b));
+              cv::resize(Depth, Depth, Depth.size()); 
+              cv::imshow("Kinect", Depth);
 	}         
 	break;
         case DepthEdge:
         { 
-	       cv::Mat DEImage = (cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
-    	       cv::Mat ucharDEImage, ucharDEImageScaled;
-               DEImage.convertTo(ucharDEImageScaled,CV_8UC1, 255, 0);
-               cv::Canny(ucharDEImageScaled, ucharDEImageScaled, max_lowThreshold, max_lowThreshold*ratio);
-               cv::imshow("Kinect", ucharDEImageScaled);
+	       /*cv::Mat DEImage = (cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
+               DEImage = DEImage(cv::Range(c, d), cv::Range(a,b));
+               cv::resize(DEImage, DEImage, DEImage.size()); 
+               DEImage.convertTo(DEImage,CV_8UC1, 255, 0);
+               cv::Canny(DEImage, DEImage, cannythresh, cannythresh*ratio);
+               cv::imshow("Kinect", DEImage);*/
+
+               cv::Mat IRImage = (cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
+               IRImage.convertTo(IRImage,CV_8UC1, 255, 0);
+               cv::equalizeHist(IRImage, IRImage);
+               cv::blur(IRImage, IRImage, cv::Size(3, 3));
+             //  cv::erode(IRImage, IRImage, cv::Mat());  
+              // pos = cv::getTrackbarPos("EdgeThresholds", "Kinect"); 
+               cv::Canny(IRImage, IRImage, cannythresh ,cannythresh*ratio);
+               IRImage = IRImage(cv::Range(c, d), cv::Range(a,b));
+               cv::resize(IRImage, IRImage, IRImage.size()); 
+               //Image zoom functionality... may want to make this seperate function
+    	       //cv::Mat ucharIRImage, ucharIRImageScaled;
+               cv::imshow("Kinect", IRImage);
         }
       	break;
         case IREdge:
 	{
                cv::Mat IRImage = (cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
-    	       cv::Mat ucharIRImage, ucharIRImageScaled;
-               IRImage.convertTo(ucharIRImageScaled,CV_8UC1, 255, 0);
-               cv::Canny(ucharIRImageScaled, ucharIRImageScaled, max_lowThreshold, max_lowThreshold*ratio);
-               cv::imshow("Kinect", ucharIRImageScaled);
+               IRImage = IRImage(cv::Range(c, d), cv::Range(a,b));
+               cv::resize(IRImage, IRImage, IRImage.size()); 
+               //Image zoom functionality... may want to make this seperate function
+    	       //cv::Mat ucharIRImage, ucharIRImageScaled;
+               IRImage.convertTo(IRImage,CV_8UC1, 255, 0);
+               cv::equalizeHist(IRImage, IRImage);
+              // cv::blur(IRImage, IRImage, cv::Size(3, 3));
+             //  cv::erode(IRImage, IRImage, cv::Mat());  
+              // pos = cv::getTrackbarPos("EdgeThresholds", "Kinect"); 
+              // cv::Canny(IRImage, IRImage, cannythresh ,cannythresh*ratio);
+               cv::imshow("Kinect", IRImage);
 	}        
         break;
     }
 
     int key = cv::waitKey(1);
-    gpioGetValue(input, &trigger);
-    if ((trigger^value) && (trigger))
+    //gpioGetValue(input, &trigger);
+  // if ((trigger^value) && (trigger))
+    if ((key & 0xFF) == 116) // T?
     {
         //gpioSetValue(redLED, on);
         //usleep(1000000);         // on for 200ms
@@ -166,8 +211,29 @@ int main(int argc, char *argv[])
         
         }
     } 
+
+   //Zoom Keys
+   if ((key & 0xFF) == 113) // Q?
+   {
+   	if (scale > 1)
+	{
+		scale--;
+	}
+   }
+   if ((key & 0xFF) == 97) // A?
+   {
+   	if (scale <= 99)
+   	{
+        	scale++;
+	}
+   }
+
+   if ((key & 0xFF) == 114) //R?
+   {
+        	scale = 100;
+   }
     protonect_shutdown = protonect_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
-    value = trigger;
+   // value = trigger;
     listener.release(frames);
     //libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100));
   }
